a07b5545cf68c917d71774ced6f4e4227a4fe350
Devin Warrick <dev.warr@gmail.com>
2019-08-26 12:21:21 -0500

    Removed Quotes model/repo/service/controller from repo

--- src/main/java/com/lambdaschool/sleepmood/repository/QuoteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lambdaschool.sleepmood.repository;
-
-import com.lambdaschool.starthere.models.Quote;
-import org.springframework.data.repository.CrudRepository;
-
-public interface QuoteRepository extends CrudRepository<Quote, Long>
-{
-
-}
